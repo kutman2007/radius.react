a1f6cdd49e675d903752e2da74b027d516680721
kutman2007 <kutnaruto@gmail.com>
2023-03-10 22:23:47 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='cub'>
+          <h1>YES</h1>
+          <div>
+          <h1 className='no'>NO</h1>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
